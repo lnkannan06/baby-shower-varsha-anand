423905a5c89300591756ba706c91fa0ef9ae75b2
lnkannan06 <119371648+lnkannan06@users.noreply.github.com>
2026-09-19 13:49:51 -0700

    config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.INVITATION = {
   // Paste the deployed Apps Script Web app URL ending in /exec here.
-  endpoint: "https://script.google.com/macros/s/YOUR-DEPLOYMENT-ID/exec",
+  endpoint: "https://script.google.com/macros/s/AKfycbxRZXJvddFlmgq1kvHh3os3xQzA-LjsXPe-2yzhVJO_avbd5hMkj4THXw7eKAoi_S4ROA/exec",
   registryUrl: "",
   location: "At Varsha & Anand’s home",
   address: "1780 Dream Catcher Ct, Sun Valley, NV 89433"
